c0ecb3a64fa59e5c984b4befd1ad13c96129822c
Espen Stokke <espenstokke98@gmail.com>
2022-12-20 09:07:25 +0100

    some experiments

--- data_acquistition/group_by_dialects.py
+++ data_acquistition/group_by_dialects.py
@@ -1,23 +1,36 @@
-from speaker_data_simplified import speakers
+# from speaker_data_simplified import speakers
 import pandas as pd
 import json
 import os
 
-data_path = 'Data\\NPSC'
+print(os.listdir())
+data_path = 'Data/NPSC'
 data_folders = os.listdir(data_path)
 
-# FUTURE: change this to include all files in data_folders
-file_path = os.path.join(data_path, data_folders[1], f"{data_folders[1]}_sentence_data.json")
+eval_folders = ["20180611", "20180201", "20170209", "20180307", "20180109"
+]
 
-f = open(file_path , encoding='utf-8')
-data = json.load(f)
+test_folders = ["20171219", "20180530", "20171122", "20170207"
+]
+train = []
+for folder in data_folders:
+    if folder not in eval_folders + test_folders:
+        train.append(folder)
 
-sentences_by_dialect = dict()
+for f in train:
+    print(f, end=", ")
+# # FUTURE: change this to include all files in data_folders
+# file_path = os.path.join(data_path, data_folders[1], f"{data_folders[1]}_sentence_data.json")
 
-for sentence in data['sentences']:
-    dialect = speakers[sentence['speaker_id']]['dialect']
-    sentence_text = sentence['sentence_text']
-    sentences_by_dialect.setdefault(dialect, []).append(sentence_text)
+# f = open(file_path , encoding='utf-8')
+# data = json.load(f)
 
-df = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in sentences_by_dialect.items() ]))
-print(df)
+# sentences_by_dialect = dict()
+
+# for sentence in data['sentences']:
+#     dialect = speakers[sentence['speaker_id']]['dialect']
+#     sentence_text = sentence['sentence_text']
+#     sentences_by_dialect.setdefault(dialect, []).append(sentence_text)
+
+# df = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in sentences_by_dialect.items() ]))
+# print(df)
